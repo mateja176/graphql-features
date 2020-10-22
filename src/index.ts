9d5dcd86f7cd1e4e49a279fb749f8ecf7d20aa75
mateja <mateja176@gmail.com>
2020-10-22 23:19:11 +0200

    writes schema based on types path and schema path

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,8 @@
 
 /* eslint-disable no-console */
 
-// import commander from 'commander';
-// import * as fs from 'fs-extra';
-// import { join } from 'path';
+import commander from 'commander';
+import fs from 'fs-extra';
 import {
   buildSchema,
   getNullableType,
@@ -16,133 +15,133 @@ import {
   GraphQLList,
   GraphQLNonNull,
   GraphQLObjectType,
-  GraphQLObjectTypeConfig,
   GraphQLOutputType,
   GraphQLSchema,
   isScalarType,
   printSchema,
 } from 'graphql';
+import { join } from 'path';
+import { Config } from './models';
 import { getFilterType, getSortType, isIdField } from './utils';
 
-// const { types } = commander
-//   .option('-t, --types', 'Path to directory containing graphql types', '.')
-//   .option('-s, --schema', 'Output path of generated schema', '.')
-//   .parse(process.argv);
+const { types, schema } = commander
+  .option('-t, --types <path>', 'Path to directory containing graphql types')
+  .option('-s, --schema <path>', 'Output path of generated schema', '.')
+  .parse(process.argv);
 
-const schemaString = `
-type User {
-  id: ID!
-  name: String!
-  age: Int!
-}
-`;
+const typesPath = join(process.cwd(), types);
 
-const schema = buildSchema(schemaString);
+const generateFeature = async (path: string) => {
+  const schemaString: string = await fs.readFile(join(typesPath, path), {
+    encoding: 'utf-8',
+  });
 
-type Config = {
-  query: GraphQLObjectTypeConfig<unknown, unknown>;
-  mutation: GraphQLObjectTypeConfig<unknown, unknown>;
-};
+  const typeSchema = buildSchema(schemaString);
 
-const config = Object.values(schema.getTypeMap())
-  .filter((type) => !isScalarType(type) && !type.name.startsWith('__'))
-  .reduce<Config>(
-    ({ query, mutation }, type) => {
-      const outputType = new GraphQLNonNull(type as GraphQLOutputType);
-      const inputType = new GraphQLNonNull(type as GraphQLInputType);
+  const config = Object.values(typeSchema.getTypeMap())
+    .filter((type) => !isScalarType(type) && !type.name.startsWith('__'))
+    .reduce<Config>(
+      ({ query, mutation }, type) => {
+        const outputType = new GraphQLNonNull(type as GraphQLOutputType);
+        const inputType = new GraphQLNonNull(type as GraphQLInputType);
 
-      const queryName = type.name.toLowerCase();
+        const queryName = type.name.toLowerCase();
 
-      const nonNullIdType = new GraphQLNonNull(GraphQLID);
+        const nonNullIdType = new GraphQLNonNull(GraphQLID);
 
-      const fieldConfigMap = (type as GraphQLObjectType).toConfig().fields;
+        const fieldConfigMap = (type as GraphQLObjectType).toConfig().fields;
 
-      const fieldConfigPairs = Object.entries(fieldConfigMap);
+        const fieldConfigPairs = Object.entries(fieldConfigMap);
 
-      const idConfigPair = fieldConfigPairs.find(([, fieldConfig]) =>
-        isIdField(fieldConfig),
-      );
-      if (!idConfigPair) {
-        throw new Error(`Type ${type.name} does not have field of type ID`);
-      }
-      const [idName] = idConfigPair;
-      const fieldConfigPairsWithoutId = fieldConfigPairs.filter(
-        ([, fieldConfig]) => !isIdField(fieldConfig),
-      );
+        const idConfigPair = fieldConfigPairs.find(([, fieldConfig]) =>
+          isIdField(fieldConfig),
+        );
+        if (!idConfigPair) {
+          throw new Error(`Type ${type.name} does not have field of type ID`);
+        }
+        const [idName] = idConfigPair;
+        const fieldConfigPairsWithoutId = fieldConfigPairs.filter(
+          ([, fieldConfig]) => !isIdField(fieldConfig),
+        );
 
-      const updateInputType = new GraphQLInputObjectType({
-        name: `Update${type.name}Input`,
-        fields: {
-          [idName]: { type: nonNullIdType },
-          ...fieldConfigPairsWithoutId.reduce(
-            (map, [name, fieldConfig]) => ({
-              ...map,
-              [name]: {
-                type: getNullableType(fieldConfig.type) as GraphQLInputType,
-              },
-            }),
-            {} as GraphQLInputFieldConfigMap,
-          ),
-        },
-      });
-
-      return {
-        query: {
-          ...query,
+        const updateInputType = new GraphQLInputObjectType({
+          name: `Update${type.name}Input`,
           fields: {
-            ...query.fields,
-            [queryName]: {
-              type: outputType,
-              args: { [idName]: { type: nonNullIdType } },
-            },
-            [`${queryName}s`]: {
-              type: new GraphQLNonNull(new GraphQLList(outputType)),
-              args: {
-                limit: { type: GraphQLInt },
-                offset: { type: GraphQLInt },
-                filter: { type: getFilterType(type.name)(fieldConfigPairs) },
-                sort: {
-                  type: getSortType(type.name)(fieldConfigPairsWithoutId),
+            [idName]: { type: nonNullIdType },
+            ...fieldConfigPairsWithoutId.reduce(
+              (map, [name, fieldConfig]) => ({
+                ...map,
+                [name]: {
+                  type: getNullableType(fieldConfig.type) as GraphQLInputType,
+                },
+              }),
+              {} as GraphQLInputFieldConfigMap,
+            ),
+          },
+        });
+
+        return {
+          query: {
+            ...query,
+            fields: {
+              ...query.fields,
+              [queryName]: {
+                type: outputType,
+                args: { [idName]: { type: nonNullIdType } },
+              },
+              [`${queryName}s`]: {
+                type: new GraphQLNonNull(new GraphQLList(outputType)),
+                args: {
+                  limit: { type: GraphQLInt },
+                  offset: { type: GraphQLInt },
+                  filter: { type: getFilterType(type.name)(fieldConfigPairs) },
+                  sort: {
+                    type: getSortType(type.name)(fieldConfigPairsWithoutId),
+                  },
                 },
               },
             },
           },
-        },
-        mutation: {
-          ...mutation,
-          fields: {
-            ...mutation.fields,
-            [`create${type.name}`]: {
-              type: outputType,
-              args: { input: { type: inputType } },
-            },
-            [`update${type.name}`]: {
-              type: outputType,
-              args: {
-                input: { type: updateInputType },
+          mutation: {
+            ...mutation,
+            fields: {
+              ...mutation.fields,
+              [`create${type.name}`]: {
+                type: outputType,
+                args: { input: { type: inputType } },
               },
-            },
-            [`delete${type.name}`]: {
-              type: outputType,
-              args: {
-                [idName]: { type: nonNullIdType },
+              [`update${type.name}`]: {
+                type: outputType,
+                args: {
+                  input: { type: updateInputType },
+                },
+              },
+              [`delete${type.name}`]: {
+                type: outputType,
+                args: {
+                  [idName]: { type: nonNullIdType },
+                },
               },
             },
           },
-        },
-      };
-    },
-    {
-      query: { name: 'Query', fields: {} },
-      mutation: { name: 'Mutation', fields: {} },
-    } as Config,
-  );
+        };
+      },
+      {
+        query: { name: 'Query', fields: {} },
+        mutation: { name: 'Mutation', fields: {} },
+      } as Config,
+    );
 
-console.log(
-  printSchema(
+  return printSchema(
     new GraphQLSchema({
       query: new GraphQLObjectType(config.query),
       mutation: new GraphQLObjectType(config.mutation),
     }),
-  ),
-);
+  );
+};
+
+fs.readdir(typesPath)
+  .then((paths) => Promise.all(paths.map(generateFeature)))
+  .then((schemaDefinitions) => {
+    fs.writeFile(join(process.cwd(), schema), schemaDefinitions[0]);
+  });
--- src/models.ts
+++ src/models.ts
@@ -1,5 +1,10 @@
-import { GraphQLFieldConfig } from 'graphql';
+import { GraphQLFieldConfig, GraphQLObjectTypeConfig } from 'graphql';
 
 export type FieldConfigPairs = ReadonlyArray<
   [string, GraphQLFieldConfig<unknown, unknown>]
 >;
+
+export type Config = {
+  query: GraphQLObjectTypeConfig<unknown, unknown>;
+  mutation: GraphQLObjectTypeConfig<unknown, unknown>;
+};
